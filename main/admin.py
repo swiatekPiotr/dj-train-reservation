5a4e8047f831d38634bc03331452f276efebde8d
swiatek1995@gmail.com <swiatek1995@gmail.com>
2022-05-25 20:43:06 +0200

    admin panel & db

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Course, Station, Timetable, Carriage, Seating
 
-# Register your models here.
+
+@admin.register(Course)
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ["id", "start", "end"]
+
+
+@admin.register(Station)
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+@admin.register(Timetable)
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ["lp_station", "stations", "courses"]
+    list_filter = ["courses"]
+
+
+@admin.register(Carriage)
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ["id", "courses"]
+
+
+@admin.register(Seating)
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ["id", "carriages"]
--- main/models.py
+++ main/models.py
@@ -10,7 +10,7 @@ class Course(models.Model):
     time = models.TimeField()
 
     def __str__(self):
-        return f"from {self.start} to {self.end}"
+        return f"from {self.start} -> to {self.end}"
 
 
 class Station(models.Model):
@@ -24,10 +24,14 @@ class Timetable(models.Model):
     courses = models.ForeignKey(Course, on_delete=models.CASCADE)
     stations = models.ForeignKey(Station, on_delete=models.CASCADE)
     lp_station = models.PositiveIntegerField()
+    at_location = models.TimeField(default=timezone.now())
 
 
 class Carriage(models.Model):
     courses = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"car number:{self.id} ({self.courses})"
 
 
 class Seating(models.Model):
