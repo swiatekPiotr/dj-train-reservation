b9b2e3e2b6fed3b6178443f5c342f62e3b3975d4
swiatek1995@gmail.com <swiatek1995@gmail.com>
2022-07-14 21:02:42 +0200

    listing of serches courses

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.core.exceptions import MultipleObjectsReturned
 from django.shortcuts import render
-from .models import Station, Timetable
+from .models import Station, Timetable, Course
 from datetime import datetime
 
 
@@ -13,7 +13,8 @@ def home(request):
 
             # Selected courses ids
             selected_courses_from = Timetable.objects.raw('SELECT * from main_timetable '
-                                                          'WHERE stations_id = %s AND at_location >= %s',
+                                                          'WHERE stations_id = %s AND at_location >= %s '
+                                                          'ORDER BY at_location ASC',
                                                           [search_from.id, search_time])
             selected_courses_to = (obj.courses_id for obj in Timetable.objects.filter(stations_id=search_to.id))
             selected_courses_id = [i.courses_id for i in selected_courses_from if i.courses_id in selected_courses_to]
@@ -22,7 +23,7 @@ def home(request):
             if str(selected_courses_id) != '[]':
                 selected_courses = []
                 for i in selected_courses_id:
-                    selected_courses.append(Timetable.objects.filter(courses_id=i))
+                    selected_courses.append(Course.objects.get(id=i))
                 return render(request, 'main/home.html', {'selected_courses': selected_courses})
 
         except (MultipleObjectsReturned, ValueError, Exception):
