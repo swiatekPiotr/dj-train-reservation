8b94433b6d3b7e468d2e71c6a9bbe50fd216cf16
swiatek1995@gmail.com <swiatek1995@gmail.com>
2022-08-05 16:58:50 +0200

    new model to reservation seat

--- main/models.py
+++ main/models.py
@@ -39,3 +39,13 @@ class Seating(models.Model):
 
     def __str__(self):
         return f"seat number: {self.id} of carriage: {self.carriages}"
+
+
+class Reservation(models.Model):
+    seats = models.ForeignKey(Seating, on_delete=models.CASCADE)
+    reservation_start_time = models.TimeField()
+    reservation_end_time = models.TimeField()
+    personality = models.CharField(max_length=80)
+
+    def __str__(self):
+        return f"{self.personality}, seat of number: {self.seats}"
